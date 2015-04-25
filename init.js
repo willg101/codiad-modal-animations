cc57edfe2242bf8545240b3c1cf8a29a670bbb12
webmaster@grandhavenchristian.org <webmaster@grandhavenchristian.org>
2015-04-25 18:24:44 -0400

    Improved animation

--- init.js
+++ init.js
@@ -23,15 +23,32 @@
         path: curpath,
 
         init: function() {
+            var duration = 300;
 
             codiad.modal.onLoadAnimation = function(){
 		$( "#modal" ).transit({
-                    scale: 1.15,
+                    scale: 0,
+                    display: 'block',
                     opacity: 0,
+                    perspective: '500px',
+                    rotateX: "90deg",
                     duration: 0, 
                }).transit({
                     scale: 1,
                     opacity: 1,
+                    perspective: '1000px',
+                    rotateX: "0deg",
+                    duration: duration,
+                    easing: 'easeOutBack'
+               });
+            };
+            codiad.modal.onUnloadAnimation = function(){
+		$( "#modal" ).transit({
+                    scale: 0,
+                    opacity: 0,
+                    perspective: '500px',
+                    rotateX: "90deg",
+                    duration: duration,
                });
             };
         },
